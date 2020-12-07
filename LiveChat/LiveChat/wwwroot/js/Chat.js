c4a206187b4606ebe9b5a88df8dc01e3b0639c5a
emircanozdag <68469546+emircanozdag@users.noreply.github.com>
2020-12-07 15:07:57 +0300

    .
    .

---
+++ LiveChat/LiveChat/wwwroot/js/Chat.js
@@ -0,0 +1,27 @@
+﻿"use strict"
+
+//import { signalR } from "./signalr/dist/browser/signalr"
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+$("#btnSend").attr("disabled", true);
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt").replace(/>/g, "&gt;");
+    var encodeMsg = user + " : " + msg;
+    $("#messageList").prepend("<li>" + encodeMsg + "</li>");
+});
+connection.start().then(function () {
+    $("#btnSend").attr("disabled", false);
+}).catch(function (err) {
+    return alert(err.toString());
+});
+$("#btnSend").on("click", function (event) {
+    var user = $("#txtUserName").val();
+    var message = $("#txtMessage").val();
+
+    if (user != "" && message != "") {
+        connection.invoke("SendMessage", user, message).catch(function (err) {
+            return alert(err.toString())
+        });
+        event.preventDefault();
+    }
+});
